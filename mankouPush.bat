c50d7cfdb4bb29ce241c45eb4af1231d71ba7a0e
mang-A43S <ningma003@gmail.com>
2013-04-05 13:57:15 +0800

    2013-04-05 周五

---
+++ mankouPush.bat
@@ -0,0 +1,14 @@
+::lastmodify 2013-3-6 08:22
+::create2013-3-2 1:21:07
+::功能：用于自动提交到github的批处理
+::使用说明：需提前设置好版本库的ogigin
+
+
+rem 设置环境变量……
+@path=%path%;"C:\WINDOWS";
+@e:
+@cd e:\ApplicationData\netDisk\klive\wiki\blog\mankou
+git add . ./
+git commit -a -m "%date%"
+git push origin master
+::pause
