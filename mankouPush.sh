9084b6ab6cc2286f1008054e022a4c0df7d51dc2
mang <ningma003@gmail.com>
2013-12-12 23:23:04 +0800

    "2013-12-12-23:23-Thursday"

--- mankouPush.sh
+++ mankouPush.sh
@@ -5,7 +5,9 @@
 #使用说明：需提前设置好版本库的ogigin
 
 cd /home/mang/AppData/Dropbox/wiki/blog/mankou/
-git add --all . 
+baseDir=$(cd "$(dirname $0)";pwd)
+#echo $baseDir
+git add --all $baseDir 
 message=`date +%Y-%m-%d-%H:%M-%A`
 message=\"$message\"
 #echo $message
