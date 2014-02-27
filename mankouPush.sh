d61ddf8d11967e17e5097235ca99269ffcb57df6
maning <mang@maningdeMacBook-Pro.local>
2014-02-27 18:53:45 +0800

    "2014-02-27-18:53-星期四"

--- mankouPush.sh
+++ mankouPush.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #功能：用于自动提交到github的批处理
 #create Thu 12 Dec 2013 11:57:25 AM CST
-#lastmodify Thu 12 Dec 2013 11:23:36 PM CST
+#lastmodify  2/27 18:52:57 2014
 #使用说明：需提前设置好版本库的ogigin
 
-cd /home/mang/AppData/Dropbox/wiki/blog/mankou/
+cd /Users/mang/AppData/Dropbox/wiki/blog/mankou.github.com
 baseDir=$(cd "$(dirname $0)";pwd)
 git add --all $baseDir 
 message=`date +%Y-%m-%d-%H:%M-%A`
