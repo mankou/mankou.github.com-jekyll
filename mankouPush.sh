679a8d67a54f399dd1145e003870b587d769e4c9
mang <ningma003@gmail.com>
2015-07-12 10:03:03 +0800

    "2015-07-12-10:03-Sunday"

--- mankouPush.sh
+++ mankouPush.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#功能：用于自动提交到github的批处理
+#鍔熻兘锛氱敤浜庤嚜鍔ㄦ彁浜ゅ埌github鐨勬壒澶勭悊
 #create Thu 12 Dec 2013 11:57:25 AM CST
 #lastmodify  2/27 18:52:57 2014
-#使用说明：需提前设置好版本库的ogigin
+#浣跨敤璇存槑锛氶渶鎻愬墠璁剧疆濂界増鏈簱鐨刼gigin
 
-cd /Users/mang/AppData/Dropbox/wiki/blog/mankou.github.com
+cd /Users/mang/AppData/蹇洏/wiki/blog/mankou.github.com
 baseDir=$(cd "$(dirname $0)";pwd)
 git add --all $baseDir 
 message=`date +%Y-%m-%d-%H:%M-%A`
